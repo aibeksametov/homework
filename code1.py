af563e555df42055359dc1766abfeaeb5ad76306
Samet Aibek <samet.aibek@bk.ru>
2023-01-31 09:03:10 +0600

    Result!

---
+++ code1.py
@@ -0,0 +1,16 @@
+age = input("Enter age:")
+if age >= "18":
+    print(f"Samet Aibek,{age} years old")
+else:
+    print("Error")
+a=5
+res=function_1(a)
+print('a = ',res)
+
+def function_1(a):
+    return a*a+a/a
+
+b=6
+result=fizika_1
+def fizika_1(b):
+    return b+b/b
